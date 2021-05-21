0b200aaf4be32862a04fb27373a35980c536a2b9
bs-puja-kumari <83964038+bs-puja-kumari@users.noreply.github.com>
2021-05-21 23:37:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 function App(){
   return (
     <div>
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+<p>Now I can render any React component on any DOM node I want using ReactDOM render</p>
     </div>
     )
 }
